61ab781e39bd14c29dea8a8b2b772d88d826b482
Rees Morris <gitkraken@rees.email>
2025-08-03 23:31:31 +0100

    Upgrade dependencies

--- apps/panel/src/views/profile-settings/personal-information/personal-information.tsx
+++ apps/panel/src/views/profile-settings/personal-information/personal-information.tsx
@@ -1,14 +1,17 @@
 import { Heading } from '@/components/ui';
+import { useFormatMessage } from '@/i18n/format-message';
 import { NameField } from './name-field';
 import { styles } from './personal-information.styles';
 
 const ID = 'personal-information';
 
 export const PersonalInformation: React.FC = () => {
+  const { formatMessage } = useFormatMessage();
+
   return (
     <section aria-labelledby={ID}>
       <Heading id={ID} level={2}>
-        Personal Information
+        {formatMessage('Personal Information')}
       </Heading>
 
       <div className={styles.grid}>
